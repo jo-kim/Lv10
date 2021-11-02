f6c24e45acf948630288c33c5f225c58ba0b89dc
PC <PC@61.82.140.244>
2021-11-02 16:09:45 +0900

    complete

--- Lv10Test_Zombie_김병조/src/controller/Main.java
+++ Lv10Test_Zombie_김병조/src/controller/Main.java
@@ -3,12 +3,12 @@ package controller;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Game.getInstance().run();
 		
 		// 시작시간 18:10
-		// 끝난시간 00:00
-		// 소요시간 00:00
+		// 끝난시간 15:30
+		// 소요시간 오래걸림
 	}
 
 }
